43adb93bbc70ffe1016b9a31d73da00e8a041473
Andrew Hexen <SyberHexen@gmail.com>
2021-12-09 05:32:27 -0800

    berlna: bootctrl: import from caf tag 'LA.UM.9.12.r1-11500-SMxx50.0'
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: If1e278cd032ccee304dd8bd303a804447c08c4a4

--- device.mk
+++ device.mk
@@ -49,13 +49,11 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-impl-wrapper.recovery \
-    android.hardware.boot@1.1-impl-wrapper \
-    android.hardware.boot@1.1-impl.recovery \
-    bootctrl.lahaina \
-    bootctrl.lahaina.recovery
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # fastbootd
 PRODUCT_PACKAGES += \
@@ -69,7 +67,6 @@ TARGET_SCREEN_WIDTH := 1080
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl \
     vendor/qcom/opensource/commonsys-intf/display
 
 # Crypto

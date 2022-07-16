6f3bd507af2b423e0d07a083efd5ba5b0adb14d4
Antonino Scordino <git@antonino.cf>
2022-07-16 16:45:38 +0200

    spacewar: Import A/B parititons & flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,19 @@ DEVICE_PATH := device/nothing/spacewar
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
